95c355fd2026bf1d5609f14b34567ac1050d140f
Miku <26039249+xMikux@users.noreply.github.com>
2023-05-23 23:05:14 +0800

    完成 implementation

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/PortableDustbin.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/PortableDustbin.java
@@ -39,7 +39,7 @@ public class PortableDustbin extends SimpleSlimefunItem<ItemUseHandler> implemen
             e.cancel();
 
             Player p = e.getPlayer();
-            p.openInventory(Bukkit.createInventory(null, 9 * 3, ChatColor.DARK_RED + "销毁物品"));
+            p.openInventory(Bukkit.createInventory(null, 9 * 3, ChatColor.DARK_RED + "刪除物品"));
             p.playSound(p.getLocation(), Sound.BLOCK_ANVIL_LAND, 1, 1);
         };
     }
