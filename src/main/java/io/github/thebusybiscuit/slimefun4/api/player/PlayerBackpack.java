b0b52121207df0f307beff55ba4d61383cc9ecb6
Miku <26039249+xMikux@users.noreply.github.com>
2023-05-25 19:28:08 +0800

    修正背包轉移問題

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/player/PlayerBackpack.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/player/PlayerBackpack.java
@@ -80,11 +80,11 @@ public class PlayerBackpack {
         String uuid = "";
 
         for (String line : item.getItemMeta().getLore()) {
-            if (line.startsWith(ChatColors.color("&7ID：")) && line.indexOf('#') != -1) {
+            if (line.startsWith(ChatColors.color("&7ID: ")) && line.indexOf('#') != -1) {
                 String[] splitLine = CommonPatterns.HASH.split(line);
 
                 if (CommonPatterns.NUMERIC.matcher(splitLine[1]).matches()) {
-                    uuid = splitLine[0].replace(ChatColors.color("&7ID："), "");
+                    uuid = splitLine[0].replace(ChatColors.color("&7ID: "), "");
                     id = OptionalInt.of(Integer.parseInt(splitLine[1]));
                 }
             }
